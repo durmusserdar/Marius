b4304801932e025a59bd5055c4bdd9c0ef3789d4
serdar <durmusserdarno@gmail.com>
2022-05-24 10:41:29 +0200

    first commit

---
+++ resources/includes.js
@@ -0,0 +1,17 @@
+$(function(){
+    $.get("resources/objects.html", data => {
+        $("*[data-object]").each((index, obj) => {
+            let object = $(obj).data("object");
+            let h = $(data).filter(`.${object}`);
+            $(obj).append(h);
+        });
+    });
+
+    $('.sidebar-toggle').click(function(){
+        $('.site').toggleClass('site-sidebar-close');
+    });
+
+    $("[data-link]").each((index, obj) =>{
+        
+    });
+});
